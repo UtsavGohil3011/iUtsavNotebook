5a9ca05233e99c1d51e8916ab8199a196562495a
Gohil Utsav A <utsav.gohil3011@gmail.com>
2024-04-16 09:26:36 +0530

    dd

--- notebook/src/Components/About.jsx
+++ notebook/src/Components/About.jsx
@@ -3,13 +3,14 @@ import Hoc from './Hoc'; // Importing the "Higher-Order Component"
 
 // Define the About component
 const About = () => {
+
+  console.log();
   return (
     <div className='text-white'>
-      <h1>This is about page</h1>
+      <h1>This is about page </h1>
     </div>
   );
 }
-
 
 // Exporting the About component wrapped with the "Higher-Order Component"
 export default Hoc(About);
